2b8a2fd0abea4c8bb7fafa2aaf0e12ce1d624ca5
Lorenzo Verri <lore.verri@gmail.com>
2023-01-20 16:54:58 +0100

    Added controller constants

---
+++ common/src/main/java/org/hikit/common/ControllerConstants.java
@@ -0,0 +1,8 @@
+package org.hikit.common;
+
+public class ControllerConstants {
+    public final static String MIN_DOCS_ON_READ = "0";
+    public final static String MAX_DOCS_ON_READ = "150";
+
+    public final static String NO_FILTERING_TOKEN = "*";
+}
