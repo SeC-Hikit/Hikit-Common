3775fdb3a60c1920c81f7772199d72ade2aca235
Lorenzo Verri <lore.verri@gmail.com>
2025-03-06 00:16:30 +0100

    feat(add-service-adapapter): add trail data and request

---
+++ common/src/main/java/org/hikit/common/dto/Coordinates2D.kt
@@ -0,0 +1,16 @@
+package org.hikit.common.dto
+
+import com.fasterxml.jackson.annotation.JsonCreator
+
+class Coordinates2D(
+    var latitude: Double = 0.0,
+    var longitude: Double = 0.0
+) {
+    @JsonCreator
+    constructor(coordinates: Array<Double>) : this() {
+        longitude = coordinates[0]
+        latitude = coordinates[1]
+    }
+
+}
+
---
+++ common/src/main/java/org/hikit/common/geo/CoordinatesRectangle.kt
@@ -0,0 +1,5 @@
+package org.hikit.common.geo
+
+import org.hikit.common.processor.Coordinates
+
+data class CoordinatesRectangle(val bottomLeft: Coordinates, val topRight: Coordinates)
